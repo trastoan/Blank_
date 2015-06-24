25afd0bfa4e52827b19ea663af205ea3e5b14bb8
Mapurunga <lucasmapurunga@gmail.com>
2015-06-24 01:22:51 -0300

    Mudei um bocado de treta no public

--- webpage/index.js
+++ webpage/index.js
@@ -5,12 +5,15 @@ var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = process.env.PORT || 3000;
 
+
+// Routing
+app.use(express.static(__dirname + '/public'));
+
+
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
 
-// Routing
-app.use(express.static(__dirname + '/public'));
 
 // Chatroom
 
